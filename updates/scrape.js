d72db652b74b5602b4a5fa8bd25e5d9e6f34333c
PradatiusD <danielprada2012@gmail.com>
2016-02-01 23:12:23 -0500

    Move scrape file

--- updates/scrape.js
+++ updates/scrape.js
@@ -1,86 +1,91 @@
-var request = require('request');
-var async   = require('async');
-var fs      = require('fs');
+function init () {
 
-var players = fs.readFileSync('tmp/players-data.json', 'utf8');
-players     = JSON.parse(players);
+  var request = require('request');
+  var async   = require('async');
+  var fs      = require('fs');
 
-function getBasicData  () {
+  var players = fs.readFileSync('tmp/players-data.json', 'utf8');
+  players     = JSON.parse(players);
 
-async.mapLimit(players, 1, function (player, callback) {
-
-  var url = "http://sflultimate.com/rereg.asp?PID="+player.pid;
-
-  request(url, function (error, response, body) {
-    if (error && response.statusCode !== 200) {
-      return callback(error, body);
-    }
-
-    console.log("Now querying "+player.firstName+" "+player.lastName)
-
-    var selectedOptionRegex = /<option value="(.*?)" selected="selected">/g
-    var emailRegex          = /<input type="text" name="email" value="(.*?)" \/>/
-
-    player.shirtSize     = selectedOptionRegex.exec(body)[1];
-    player.skillLevel    = selectedOptionRegex.exec(body)[1];
-    player.participation = selectedOptionRegex.exec(body)[1];
-    player.email         = emailRegex.exec(body)[1];
-
-    callback(null, player);
-  })
-
-}, function (err, players) {
-  fs.writeFileSync('tmp/players-data.json',JSON.stringify(players));
-});
-
-}
-
-function getStats() {
+  function getBasicData  () {
 
   async.mapLimit(players, 1, function (player, callback) {
 
-    var url = "http://sflultimate.com/bio.asp?ID="+player.pid;
+    var url = "http://sflultimate.com/rereg.asp?PID="+player.pid;
 
-    console.log("Requesting: "+url);
-
-    request(url, function (err, response, body) {
-
-      var tagRegex = /<span .*?>(.*?)<\/span.*?>/g
-
-      var match;
-      var matches = [];
-      var i = -1;
-
-      while ((match = tagRegex.exec(body)) !== null) {
-        matches.push(match[1]);
+    request(url, function (error, response, body) {
+      if (error && response.statusCode !== 200) {
+        return callback(error, body);
       }
 
-      // Remove columns
-      matches = matches.splice(4);
+      console.log("Now querying "+player.firstName+" "+player.lastName)
 
-      var scores = [];
+      var selectedOptionRegex = /<option value="(.*?)" selected="selected">/g
+      var emailRegex          = /<input type="text" name="email" value="(.*?)" \/>/
 
-      while (matches.length) {
-        
-        var score = matches.splice(0,4);
-        scores.push({
-          date:     score[0],
-          scores:   parseInt(score[1]),
-          assists:  parseInt(score[2]),
-          defenses: parseInt(score[3])
-        });
-      }
+      player.shirtSize     = selectedOptionRegex.exec(body)[1];
+      player.skillLevel    = selectedOptionRegex.exec(body)[1];
+      player.participation = selectedOptionRegex.exec(body)[1];
+      player.email         = emailRegex.exec(body)[1];
 
-      player.scores = scores;
       callback(null, player);
-    });
-  }, function (err, players) {
-    console.log("Completed");
-    fs.writeFileSync('tmp/players-data-with-scores.json',JSON.stringify(players));
-  });
-}
+    })
 
-getStats();
+  }, function (err, players) {
+    fs.writeFileSync('tmp/players-data.json',JSON.stringify(players));
+  });
+
+  }
+
+  function getStats() {
+
+    async.mapLimit(players, 1, function (player, callback) {
+
+      var url = "http://sflultimate.com/bio.asp?ID="+player.pid;
+
+      console.log("Requesting: "+url);
+
+      request(url, function (err, response, body) {
+
+        var tagRegex = /<span .*?>(.*?)<\/span.*?>/g
+
+        var match;
+        var matches = [];
+        var i = -1;
+
+        while ((match = tagRegex.exec(body)) !== null) {
+          matches.push(match[1]);
+        }
+
+        // Remove columns
+        matches = matches.splice(4);
+
+        var scores = [];
+
+        while (matches.length) {
+          
+          var score = matches.splice(0,4);
+          scores.push({
+            date:     score[0],
+            scores:   parseInt(score[1]),
+            assists:  parseInt(score[2]),
+            defenses: parseInt(score[3])
+          });
+        }
+
+        player.scores = scores;
+        callback(null, player);
+      });
+    }, function (err, players) {
+      console.log("Completed");
+      fs.writeFileSync('tmp/players-data-with-scores.json',JSON.stringify(players));
+    });
+  }
+
+  getStats();
+
+
+}
 
 /*
  
