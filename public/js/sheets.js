7dc86d8ede82994cc9e9de23982d70814ece013e
PradatiusD <danielprada2012@gmail.com>
2016-10-17 14:53:59 -0400

    First draft of automatic stat sheets

---
+++ public/js/sheets.js
@@ -0,0 +1,34 @@
+(function () {
+
+var app = angular.module("StatSheetApp",[]);
+
+app.controller("StatSheetController",function ($scope, $http) {
+
+    var url = "/teams?f=json";
+    var query = $http.get(url);
+
+    query.then(function (response) {
+
+      var data    = response.data;
+      var teams   = data.teams;
+      var players = data.players;
+
+      teams.forEach(function (team) {
+
+        team.players = team.players.map(function (playerId) {
+          for (var i = 0; i < players.length; i++) {
+            if (players[i]._id === playerId) {
+              playerId = players[i];
+              break;
+            }
+          }
+          return playerId;
+        });
+      });
+
+
+      $scope.teams = teams;
+    });
+});
+
+})();
