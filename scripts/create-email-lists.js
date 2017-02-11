5e8d2a44080e5651d68685fdc62a1df2d9a7990b
PradatiusD <danielprada2012@gmail.com>
2017-02-11 11:36:43 -0500

    Updates to now have registration

---
+++ scripts/create-email-lists.js
@@ -0,0 +1,25 @@
+
+
+// To captain
+// send email of people
+
+db.teams.find({league:ObjectId('589f1ea1e233f50400259325')}).forEach(function (team) {
+  
+  var email = '';
+
+  var playerEmails = '';
+
+
+  db.players.find({_id: {$in: team.players}}).forEach(function (player) {
+
+    if (player._id.valueOf() === team.captains[0].valueOf()) {
+      email += '\nTo: ' + player.email;
+      email += '\nSubject: '+team.color+' Team'
+    }
+
+    playerEmails += '\n<' + player.name.first + ' ' + player.name.last + '> ' + player.email;
+  });
+
+  print(email);
+  print(playerEmails);
+});
