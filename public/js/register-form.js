bf9921a1941a515d843012d0e0e49c27fc246ff9
Daniel Prada <daniel@lamusica.com>
2018-12-18 10:15:43 -0500

    Adding skill level and checkbox changes price on date selection

--- public/js/register-form.js
+++ public/js/register-form.js
@@ -1,88 +1,39 @@
 braintree.setup(clientToken, "dropin", {
-  container: "payment-form"
+    container: "payment-form"
 });
 
 (function ($) {
-  $.get('/players', function (playerData) {
+    
+    var registrationCheckboxes = $("#registration_dates input");
 
-    playerData = playerData.map(function (d){
-      d.name = d.full_name.first +" "+d.full_name.last;
-      return d;
-    });
+    registrationCheckboxes.change(function () {
+        
+        var datesPicked = 0;
 
-    $("#firstName").typeahead({
-      source: playerData,
-      afterSelect: function (d) {
-        var updates = [
-          {
-            id: "#firstName",
-            value: d.full_name.first,
-          },{
-            id: "#lastName",
-            value: d.full_name.last
-          },{
-            id: "#email",
-            value: d.email
-          },{
-            id: "#user_id",
-            value: d.id
-          },{
-            id: "#shirtSize",
-            value: d.size
-          },{
-            id: "#skillLevel",
-            value: d.skill
-          },{
-            id: "#age",
-            value: d.age || "Adult"
-          },{
-            id: "#participation",
-            value: d.participation || "80"
-          }
-        ];
-
-        updates.forEach(function (update) {
-
-          if (update.id === "#age") {
-            $("#age").find('input').each(function () {
-              if (update.value === $(this).val()) {
-                $(this).attr('checked','');
-              }
-            });
-          } else {
-            $(update.id).val(update.value);          
-          }
-
+        registrationCheckboxes.each(function () {
+            if($(this).is(":checked")){
+                datesPicked += 1;
+            }
         });
-      }
-    })
-
-    $("#partner").typeahead({
-      source: playerData,
-      afterSelect: function (d) {
-        $('#partner_id').val(d.id);
-      }
+        
+        var $totalCost = $("#total_cost");
+        
+        switch (datesPicked) {
+            case 0:
+                $totalCost.text("N/A, please pick a date");
+                break;
+            case 1:
+                $totalCost.text("$20.00");
+                break;
+            case 2:
+                $totalCost.text("$40.00");
+                break;
+            case 3:
+                $totalCost.text("$50.00");
+                break;
+        }
+        
+        
     });
 
-    function validateIfOnDB () {
-      var fullname = $("#firstName").val()+" "+$("#lastName").val();
-      var userId   = $("#user_id").val();
-
-      var found = false;
-
-      for (var i = 0; i < playerData.length; i++) {
-        if(playerData[i].name === fullname) {
-          found = true;
-          break;
-        }
-      };
-
-      if (!found) {
-        $("#user_id").val("");
-      }
-    }
-
-    $("#firstName").change(validateIfOnDB);
-    $("#lastName").change(validateIfOnDB);
-  });
 })(jQuery);
