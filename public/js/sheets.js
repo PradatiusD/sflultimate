2a1fd953e14ed10d22d50cb4b0b4b2508b730345
Daniel Prada <DanielPrada2012@gmail.com>
2021-10-06 15:20:00 -0400

    Fixing sheet format

--- public/js/sheets.js
+++ public/js/sheets.js
@@ -1,34 +1,28 @@
 (function () {
+  var app = angular.module('StatSheetApp', [])
 
-var app = angular.module("StatSheetApp",[]);
-
-app.controller("StatSheetController",function ($scope, $http) {
-
-    var url = "/teams?f=json";
-    var query = $http.get(url);
+  app.controller('StatSheetController', function ($scope, $http) {
+    var url = '/teams?f=json'
+    var query = $http.get(url)
 
     query.then(function (response) {
-
-      var data    = response.data;
-      var teams   = data.teams;
-      var players = data.players;
+      var data = response.data
+      var teams = data.teams
+      var players = data.players
 
       teams.forEach(function (team) {
-
         team.players = team.players.map(function (playerId) {
           for (var i = 0; i < players.length; i++) {
             if (players[i]._id === playerId) {
-              playerId = players[i];
-              break;
+              playerId = players[i]
+              break
             }
           }
-          return playerId;
-        });
-      });
+          return playerId
+        })
+      })
 
-
-      $scope.teams = teams;
-    });
-});
-
-})();
+      $scope.teams = teams
+    })
+  })
+})()
