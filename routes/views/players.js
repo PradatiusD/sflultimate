1003fcbb6efa1d4bd4e803d523a3768e1c006eb2
Daniel Prada <DanielPrada2012@gmail.com>
2021-10-06 01:03:27 -0400

    Added rostering check

--- routes/views/players.js
+++ routes/views/players.js
@@ -1,6 +1,7 @@
 const keystone = require('keystone')
 const Player = keystone.list('Player')
 const League = keystone.list('League')
+const Team = keystone.list('Team')
 
 module.exports = async function (req, res) {
   const activeLeague = await League.model.findOne({ isActive: true }).lean().exec()
@@ -9,6 +10,19 @@ module.exports = async function (req, res) {
       $in: [activeLeague._id]
     }
   }
-  const players = await Player.model.find(query, { password: 0, email: 0 })
+
+  const teams = await Team.model.find().where('league', activeLeague._id).lean().exec()
+  const map = {}
+  for (const team of teams) {
+    for (const player of team.players) {
+      map[player.valueOf()] = team._id
+    }
+  }
+
+  const players = await Player.model.find(query, { password: 0, email: 0, isAdmin: 0 }).lean().exec()
+  for (const player of players) {
+    player.team = map[player._id.valueOf()]
+  }
+
   res.json(players)
 }
