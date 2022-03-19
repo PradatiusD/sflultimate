9244656ef6e0c485010acf5aa4fe903a5cd29f79
Daniel Prada <DanielPrada2012@gmail.com>
2022-03-19 07:43:20 -0400

    Added enhancements to draftboard for analytics

--- public/js/draftboard.js
+++ public/js/draftboard.js
@@ -56,26 +56,88 @@
       }
     }
 
-    $scope.keysForTotals = ['gender', 'shirtSize', 'shirtSizeWithGender', 'teamColorAndNameWithShirtSizeAndGender', 'participation', 'insuranceGroup']
+    $scope.columnsForTotals = {
+      gender: ['Gender'],
+      shirtSize: ['Shirt Size'],
+      shirtSizeWithGender: ['Shirt Size', 'Gender'],
+      teamColorAndNameWithShirtSizeAndGender: ['Team Name', 'Team Color', 'Size', 'Gender'],
+      participation: ['Participation %'],
+      insuranceGroup: ['Age Range'],
+      playersPerTeam: ['Number of Teams'],
+      playersPerTeamWithAttendance: ['Number of Teams'],
+      genderPerTeam: ['Number of Teams', 'Gender'],
+      genderPerTeamWithAttendance: ['Number of Teams', 'Gender']
+    }
+
+    $scope.keysForTotals = Object.keys($scope.columnsForTotals)
 
     query.success(function (players) {
-      const totals = {}
+      const totals = {
+        playersPerTeam: {},
+        playersPerTeamWithAttendance: {},
+        genderPerTeam: {},
+        genderPerTeamWithAttendance: {}
+      }
 
+      const teamSizes = [4, 5, 6, 7, 8]
+      for (const countKey in totals) {
+        teamSizes.forEach(function (teamsAmount) {
+          if (!countKey.startsWith('gender')) {
+            totals[countKey][teamsAmount] = 0
+          }
+        })
+      }
+
+      const JOIN_CHAR = '|'
       players.forEach(function (player) {
+        teamSizes.forEach(function (teamsAmount) {
+          const perTeamScore = 1 / teamsAmount
+          const perTeamScoreWeightedWithParticipation = (perTeamScore * parseInt(player.participation) / 100)
+          const teamNumberAndGenderKey = [teamsAmount, player.gender].join(JOIN_CHAR)
+
+          totals.playersPerTeam[teamsAmount] += perTeamScore
+          totals.playersPerTeamWithAttendance[teamsAmount] += perTeamScoreWeightedWithParticipation
+
+          if (!totals.genderPerTeam[teamNumberAndGenderKey]) {
+            totals.genderPerTeam[teamNumberAndGenderKey] = 0
+          }
+
+          if (!totals.genderPerTeamWithAttendance[teamNumberAndGenderKey]) {
+            totals.genderPerTeamWithAttendance[teamNumberAndGenderKey] = 0
+          }
+
+          totals.genderPerTeam[teamNumberAndGenderKey] += perTeamScore
+          totals.genderPerTeamWithAttendance[teamNumberAndGenderKey] += perTeamScoreWeightedWithParticipation
+        })
+
         $scope.keysForTotals.forEach(function (key) {
           if (!totals[key]) {
             totals[key] = {}
           }
           const totalKey = totals[key]
+          let value = player[key]
           if (key === 'shirtSizeWithGender') {
-            player[key] = [player.shirtSize, player.gender].join('-')
+            value = [
+              player.shirtSize,
+              player.gender
+            ].join(JOIN_CHAR)
           }
 
           if (key === 'teamColorAndNameWithShirtSizeAndGender') {
             if (player.team) {
-              player[key] = [player.team.name, player.team.color, player.shirtSize, player.gender].join('-')
+              value = [
+                player.team.name,
+                player.team.color,
+                player.shirtSize,
+                player.gender
+              ].join(JOIN_CHAR)
             } else {
-              player[key] = ['No Team', 'No Color', player.shirtSize, player.gender].join('-')
+              value = [
+                'No Team',
+                'No Color',
+                player.shirtSize,
+                player.gender
+              ].join(JOIN_CHAR)
             }
           }
 
@@ -83,20 +145,22 @@
             const age = player.age
             // <12 13-15 16-19 20+
             if (age <= 12) {
-              player[key] = '12 and under'
+              value = '12 and under'
             } else if (age <= 15) {
-              player[key] = '13-15'
+              value = '13-15'
             } else if (age <= 19) {
-              player[key] = '16-19'
+              value = '16-19'
             } else {
-              player[key] = '20+'
+              value = '20+'
             }
           }
 
-          if (!totalKey[player[key]]) {
-            totalKey[player[key]] = 0
+          if (value) {
+            if (!totalKey[value]) {
+              totalKey[value] = 0
+            }
+            totalKey[value]++
           }
-          totalKey[player[key]]++
         })
       })
 
