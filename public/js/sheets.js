945cd5287dd220fa6459aac090ede5d355691292
Daniel Prada <DanielPrada2012@gmail.com>
2022-03-20 13:41:15 -0400

    Added current game/time opponent onto page

--- public/js/sheets.js
+++ public/js/sheets.js
@@ -2,27 +2,47 @@
   var app = angular.module('StatSheetApp', [])
 
   app.controller('StatSheetController', function ($scope, $http) {
-    var url = '/teams?f=json'
-    var query = $http.get(url)
+    let teamsResponse
+    let scheduleResponse
+    $http.get('/teams?f=json')
+      .then(function (response) {
+        teamsResponse = response
+        return $http.get('/schedule?f=json')
+      }).then(function (response) {
+        scheduleResponse = response
+        const teamsData = teamsResponse.data
+        const teams = teamsData.teams
+        const players = teamsData.players
 
-    query.then(function (response) {
-      var data = response.data
-      var teams = data.teams
-      var players = data.players
-
-      teams.forEach(function (team) {
-        team.players = team.players.map(function (playerId) {
-          for (var i = 0; i < players.length; i++) {
-            if (players[i]._id === playerId) {
-              playerId = players[i]
-              break
-            }
+        const scheduleMap = {}
+        scheduleResponse.data.games.forEach(function (game) {
+          const isGameToday = new Date(game.scheduledTime).toDateString() === new Date().toDateString()
+          if (!isGameToday) {
+            return
           }
-          return playerId
+          game.scheduledTime = new Date(game.scheduledTime)
+          scheduleMap[game.homeTeam] = game
+          scheduleMap[game.awayTeam] = game
         })
-      })
 
-      $scope.teams = teams
-    })
+        $scope.teamMap = {}
+
+        teams.forEach(function (team) {
+          $scope.teamMap[team._id] = team.name
+          team.game = scheduleMap[team._id]
+          team.players = team.players.map(function (playerId) {
+            for (let i = 0; i < players.length; i++) {
+              if (players[i]._id === playerId) {
+                playerId = players[i]
+                break
+              }
+            }
+            return playerId
+          })
+        })
+
+        console.log(teams)
+        $scope.teams = teams
+      })
   })
 })()
