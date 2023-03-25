e9684c9914e13a61056e3e9bbad3df59fa82e91f
Daniel Prada <DanielPrada2012@gmail.com>
2023-03-25 09:33:51 -0400

    Fixing registration level for student display and script for undrafted players

--- scripts/create-email-list-of-not-registered.js
+++ scripts/create-email-list-of-not-registered.js
@@ -1,18 +1,34 @@
-// mongo mongodb://localhost/sflultimate
+/**
+ * mongo sflultimate scripts/create-email-list-of-not-registered.js
+ */
 
 var activeLeague = db.leagues.findOne({ isActive: true })
 
-var registeredEmails = db.players.find({
+var registeredPlayers = db.players.find({
   leagues: {
     $in: [activeLeague._id]
   }
 }, { email: 1, leagues: 1 }).toArray().map(function (player) {
-  return player.email
+  return player
 })
 
-var allPlayers = db.players.find({}, { email: 1 }).toArray().map(p => p.email)
-var allHatterPlayers = db.players.find({}, { email: 1 }).toArray().map(p => p.email)
-var combinedPlayers = allPlayers.concat(allHatterPlayers).filter(function (email, index, self) {
-  return registeredEmails.indexOf(email) === -1 && self.indexOf(email) === index
+var playerIDsOnTeams = []
+var teams = db.teams.find({
+  league: activeLeague._id
+}).toArray().forEach(function (team) {
+  team.players.forEach(function (player) {
+    playerIDsOnTeams.push(player.valueOf())
+  })
 })
-printjson(combinedPlayers)
+
+// var allPlayers = db.players.find({}, { email: 1 }).toArray().map(p => p.email)
+// // var allHatterPlayers = db.players.find({}, { email: 1 }).toArray().map(p => p.email)
+// var combinedPlayers = allPlayers.filter(function (email, index, self) {
+//   return registeredEmails.indexOf(email) === -1 && self.indexOf(email) === index
+// })
+var playersNotOnTeams = registeredPlayers.filter(function (player) {
+  return playerIDsOnTeams.indexOf(player._id.valueOf()) === -1
+}).map(function (player) {
+  return player.email
+})
+printjson(playersNotOnTeams.join(', '))
