6ff2936785b81bf294f6c854aafe3397f23f2cf8
Daniel Prada <danielprada2012@gmail.com>
2026-01-08 17:05:30 -0500

    Added countdown

--- app/next/components/Notification.js
+++ app/next/components/Notification.js
@@ -1,16 +1,26 @@
 'use client'
 import Countdown from 'react-countdown'
+import { useEffect, useState } from 'react'
 
 export default function Notification () {
-  const registrationCloseDate = new Date('2025-10-05T03:59:59.000Z')
-  if (registrationCloseDate.getTime() < Date.now()) {
+  const [pathname, setPathname] = useState('')
+  useEffect(() => {
+    setPathname(window.location.pathname)
+  }, [])
+
+  const config = {
+    title: 'Winter Beach League Registration',
+    url: '/leagues/winter-beach-league-2026/register'
+  }
+  const registrationCloseDate = new Date('2026-01-09T04:59:00.000Z')
+  if (registrationCloseDate.getTime() < Date.now() || pathname === config.url) {
     return <></>
   }
   return (
     <div className="container">
       <div className="alert alert-info" role="alert">
         <span className="glyphicon glyphicon-info-sign" style={{ position: 'relative', top: '2px' }}></span>{' '}
-        <strong><a href="/register">Fall League Registration</a> closes soon:</strong>
+        <strong><a href={config.url}></a>{config.title} closes <u>soon</u>:</strong>
         {' '} only{' '}
         <Countdown
           date={registrationCloseDate.getTime()}
@@ -30,7 +40,7 @@ export default function Notification () {
             )
           }}
         />
-        {' '}left before you can <a href="/register">sign up</a>!
+        {' '}left before you can <a href={config.url}>sign up</a>!
       </div>
     </div>
 
